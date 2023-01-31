640b6b9eebdf76c5979d86eaf66ae8ceefe45a52
Pratik Baravkar <pratik.baravkar@joshsoftware.com>
2023-01-31 15:34:57 +0530

    routes added for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    devise_for :users, path: "", path_names: {
-                         sign_in: "login",
-                         sign_out: "logout",
-                         registration: "signup",
-                       },
-                       controllers: {
-                         sessions: "users/sessions",
-                         registrations: "users/registrations",
-                       }
-  end
-  resources :users
+  devise_for :users, path: "", path_names: {
+                       sign_in: "login",
+                       sign_out: "logout",
+                       registration: "signup",
+                     },
+                     controllers: {
+                       sessions: "users/sessions",
+                       registrations: "users/registrations",
+                     }
+  resources :users, :admissions, :addresses, :medicines, :rooms
+  post 'treatments/add' to: 'theatments#add'
 end
